51fb205e356d3130a48b5868d0adddb9ecac9d61
Leoshirei <routa1082@gmail.com>
2023-03-28 10:24:39 +0200

    kalkulator

--- nauka/Program.cs
+++ nauka/Program.cs
@@ -4,8 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("HI");
+            int a, b, wyb;
+            Console.WriteLine("KALKULATOR");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Podaj pierwszą liczbę: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Co chcesz zrobić?");
+            Console.WriteLine("1. Dodawanie");
+            Console.WriteLine("2. Odejmowanie");
+            Console.WriteLine("3. Mnożenie");
+            Console.WriteLine("4. Dzielenie");
+            Console.WriteLine("-----------------------");
+            wyb = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            switch (wyb)
+            {
+                case 1:
+                    Console.WriteLine("Wynik dodawania: " + (a + b));
+                    break;
+                case 2:
+                    Console.WriteLine("Wynik odejmowania: " + (a - b));
+                    break;
+                case 3:
+                    Console.WriteLine("Wynik mnożenia: " + (a * b));
+                    break;
+                case 4:
+                    Console.WriteLine("Wynik dzielenia: " + (a / b));
+                    break;
+            }
         }
     }
 }
